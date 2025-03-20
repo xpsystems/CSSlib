e715ad82e7c4caeb0f70f4be45b00a2c0549a98f
Michaelninder <fabianeternis@gmail.com>
2025-03-20 08:14:00 +0100

    Complete Darkmode (for now)

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,21 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll("[data-modal]").forEach(button => {
-        const modalId = button.getAttribute("data-modal");
-        const modal = document.getElementById(modalId);
-        const closeModalBtn = modal.querySelector(".modal-close");
-
-        button.addEventListener("click", function () {
-            modal.classList.add("active");
-        });
-
-        closeModalBtn.addEventListener("click", function () {
-            modal.classList.remove("active");
-        });
-
-        window.addEventListener("click", function (event) {
-            if (event.target === modal) {
-                modal.classList.remove("active");
-            }
-        });
+      const modalId = button.getAttribute("data-modal");
+      const modal = document.getElementById(modalId);
+      const closeModalBtn = modal.querySelector(".modal-close");
+      const modalOverlay = document.getElementById("modalOverlay");
+  
+      button.addEventListener("click", function () {
+        modal.classList.add("active");
+        modalOverlay.classList.add("active");
+      });
+  
+      closeModalBtn.addEventListener("click", function () {
+        modal.classList.remove("active");
+        modalOverlay.classList.remove("active");
+      });
+  
+      window.addEventListener("click", function (event) {
+        if (event.target === modalOverlay) {
+          modal.classList.remove("active");
+          modalOverlay.classList.remove("active");
+        }
+      });
     });
-});
+  });
+  
