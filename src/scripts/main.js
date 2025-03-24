81b1e439857e0bc1c4edd0c7e87e70b9df9f4798
Michaelninder <fabianeternis@gmail.com>
2025-03-24 13:45:01 +0100

    https://cdn.jsdelivr.net/ Test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,14 @@ import './tooltips.js';
 import './modal.js';
 import "./darkmode.js";
 import "./scrolltotop.js";
+import "https://cdn.jsdelivr.net/gh/xpsystems/CSSlib/src/scripts//preload.js";
+import "https://cdn.jsdelivr.net/gh/xpsystems/CSSlib/src/scripts//darkmode.js";
+import 'https://cdn.jsdelivr.net/gh/xpsystems/CSSlib/src/scripts//alerts.js';
+import 'https://cdn.jsdelivr.net/gh/xpsystems/CSSlib/src/scripts//dropdowns.js';
+import 'https://cdn.jsdelivr.net/gh/xpsystems/CSSlib/src/scripts//tooltips.js';
+import 'https://cdn.jsdelivr.net/gh/xpsystems/CSSlib/src/scripts//modal.js';
+import "https://cdn.jsdelivr.net/gh/xpsystems/CSSlib/src/scripts//darkmode.js";
+import "https://cdn.jsdelivr.net/gh/xpsystems/CSSlib/src/scripts//scrolltotop.js";
 
 
 console.log("CSSlib Loaded Successfully!");
